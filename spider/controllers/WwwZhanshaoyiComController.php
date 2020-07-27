da246ce698ef1374495e9817b11cf1fe4f1b4d3e
microsoft <nativescript@outlook.com>
2020-07-27 09:03:00 +0800

    no message

--- spider/controllers/WwwZhanshaoyiComController.php
+++ spider/controllers/WwwZhanshaoyiComController.php
@@ -4,10 +4,10 @@ namespace spider\controllers;
 
 
 use Yii;
-
 use GuzzleHttp\Client;
 use GuzzleHttp\Promise;
 use QL\QueryList;
+use common\models\SoftwareCategory;
 
 
 class WwwZhanshaoyiComController extends BaseController
@@ -22,10 +22,43 @@ class WwwZhanshaoyiComController extends BaseController
         $url = 'http://www.zhanshaoyi.com/rjxz.html';
 
         $rules = [
-            'name' => ['strong', 'text'],
+            'name' => ['', 'text'],
         ];
 
         $range = 'div.entry-content h2';
+
+        $queryList = QueryList::get($url)
+            ->range($range)
+            ->rules($rules)
+            ->query()
+            ->getData();
+
+        $rawDataList = $queryList->all();
+
+        dd($rawDataList);
+
+//        foreach ($rawDataList as $rawData) {
+//            $softwareCategory = new SoftwareCategory();
+//            $softwareCategory->title = $rawData['name'];
+//            $softwareCategory->save(false);
+//        }
+
+    }
+
+
+    public function actionSoftware()
+    {
+
+        ini_set('max_execution_time', 0);
+
+        $url = 'http://www.zhanshaoyi.com/rjxz.html';
+
+        $rules = [
+            'title' => ['strong', 'text'],
+            'software' => ['', 'html'],
+        ];
+
+        $range = 'div.entry-content p';
 
         $queryList = QueryList::get($url)
             ->range($range)
