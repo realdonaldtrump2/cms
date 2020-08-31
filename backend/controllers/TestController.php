5c37bfec274ddb1552d7a94a3aae2090da386f4d
microsoft <nativescript@outlook.com>
2020-08-31 08:45:40 +0800

    no message

--- backend/controllers/TestController.php
+++ backend/controllers/TestController.php
@@ -17,10 +17,22 @@ class TestController extends BaseController
 
         $reader = new JsonReader();
         $reader->open("http://backend.cms.com/zhidao_qa.json");
-        while ($reader->read("_id")) {
+        while ($reader->read("type")) {
             var_dump($reader->value());
-            exit();
         }
+        $reader->close();
+
+//        $reader->close();
+
+//        $reader = new pcrov\JsonReader\JsonReader();
+//        $reader->open("data.json");
+//        while ($reader->read()) {
+//            $name = $reader->name();
+//            if ($name !== null) {
+//                echo "$name: {$reader->value()}\n";
+//            }
+//        }
+//        $reader->close();
 
     }
 
